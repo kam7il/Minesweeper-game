d9e8301abcf51dae72058ffe8741d414390b2984
Kam7il <118853875+kam7il@users.noreply.github.com>
2023-01-06 13:10:00 +0100

    Add files via upload

---
+++ Saper/Saper/FormSaper.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Saper
+{
+    partial class FormSaper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.graToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.małaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.średniaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dużaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelGameButtons = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(340, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // graToolStripMenuItem
+            // 
+            this.graToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.małaToolStripMenuItem,
+            this.średniaToolStripMenuItem,
+            this.dużaToolStripMenuItem});
+            this.graToolStripMenuItem.Name = "graToolStripMenuItem";
+            this.graToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.graToolStripMenuItem.Text = "Gra";
+            // 
+            // małaToolStripMenuItem
+            // 
+            this.małaToolStripMenuItem.Name = "małaToolStripMenuItem";
+            this.małaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.małaToolStripMenuItem.Text = "Mała";
+            this.małaToolStripMenuItem.Click += new System.EventHandler(this.małaToolStripMenuItem_Click);
+            // 
+            // średniaToolStripMenuItem
+            // 
+            this.średniaToolStripMenuItem.Name = "średniaToolStripMenuItem";
+            this.średniaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.średniaToolStripMenuItem.Text = "Średnia";
+            this.średniaToolStripMenuItem.Click += new System.EventHandler(this.średniaToolStripMenuItem_Click);
+            // 
+            // dużaToolStripMenuItem
+            // 
+            this.dużaToolStripMenuItem.Name = "dużaToolStripMenuItem";
+            this.dużaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dużaToolStripMenuItem.Text = "Duża";
+            this.dużaToolStripMenuItem.Click += new System.EventHandler(this.dużaToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panelGameButtons.AutoSize = true;
+            this.panelGameButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelGameButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelGameButtons.Location = new System.Drawing.Point(0, 24);
+            this.panelGameButtons.Name = "panel1";
+            this.panelGameButtons.Size = new System.Drawing.Size(340, 299);
+            this.panelGameButtons.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(340, 323);
+            this.Controls.Add(this.panelGameButtons);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Saper v0.1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem graToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem małaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem średniaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dużaToolStripMenuItem;
+        private System.Windows.Forms.Panel panelGameButtons;
+    }
+}
+
---
+++ Saper/Saper/FormSaper.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class FormSaper : Form
+    {
+        enum GameSize
+        {
+            Small, Medium, Big
+        };
+        const int fieldSize = 30;
+        Random generator = new Random();
+
+        int widthMap;
+        int heightMap;
+        int minesMap;
+        int openToWin;
+        Button[,] fieldsMap;
+
+        GameSize gameSize;
+
+        public FormSaper()
+        {
+            InitializeComponent();
+
+            gameSize = GameSize.Small;
+            małaToolStripMenuItem_Click(null, null);
+        }
+
+        private void Generate(int widthMap, int heightMap, int minesMap)
+        {
+            panelGameButtons.Controls.Clear();
+            this.widthMap = widthMap;
+            this.heightMap = heightMap;
+            this.minesMap = minesMap;
+            this.openToWin = widthMap * heightMap - minesMap;
+            fieldsMap = new Button[widthMap, heightMap];
+
+            //generowanie buttonów
+            for (int x = 0; x < widthMap; x++)
+            {
+                for (int y = 0; y < heightMap; y++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(fieldSize, fieldSize);
+                    b.Location = new Point(fieldSize * x, fieldSize * y);
+                    fieldsMap[x, y] = b;
+                    b.Click += Field_Click;
+                    panelGameButtons.Controls.Add(b);
+                }
+            }
+            //losowanie min
+            do
+            {
+                int x = generator.Next(widthMap);
+                int y = generator.Next(heightMap);
+                if (fieldsMap[x, y].Name != "@")
+                {
+                    fieldsMap[x, y].Name = "@";
+                    minesMap--;
+                }
+            } while (minesMap > 0);
+            //obliczanie ilosci min w sąsiedztwie
+            for (int x = 0; x < widthMap; x++)
+            {
+                for (int y = 0; y < heightMap; y++)
+                {
+                    if (fieldsMap[x, y].Name != "@")
+                    {
+                        int mineCount = 0;
+                        for (int xx = x - 1; xx <= x + 1; xx++)
+                        {
+                            for (int yy = y - 1; yy <= y + 1; yy++)
+                            {
+                                if (xx >= 0 && xx < widthMap &&
+                                   yy >= 0 && yy < heightMap &&
+                                   fieldsMap[xx, yy].Name == "@")
+                                {
+                                    mineCount++;
+                                }
+                            }
+                        }
+                        if (mineCount > 0)
+                        {
+                            fieldsMap[x, y].Name = mineCount.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Field_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (b.Name == "@")
+            {
+                EndGame("Niestety przegrałeś");
+            }
+            else
+            {
+                for (int x = 0; x < widthMap; x++)
+                {
+                    for (int y = 0; y < heightMap; y++)
+                    {
+                        if (fieldsMap[x, y] == b)
+                        {
+                            OpenField(x, y);
+                        }
+                    }
+                }
+            }
+            if (openToWin == 0)
+            {
+                EndGame("Brawo - wygrałeś!");
+            }
+        }
+
+        private void EndGame(string info)
+        {
+            OpenAllFields();
+            if (MessageBox.Show("Czy chcesz jeszce jeden raz?",
+                               info,
+                               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                switch (gameSize)
+                {
+                    case GameSize.Small: małaToolStripMenuItem_Click(null, null); break;
+                    case GameSize.Medium: średniaToolStripMenuItem_Click(null, null); break;
+                    case GameSize.Big: dużaToolStripMenuItem_Click(null, null); break;
+                }
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void OpenField(int x, int y)
+        {
+            if (fieldsMap[x, y].BackColor != Color.White)
+            {
+                fieldsMap[x, y].Text = fieldsMap[x, y].Name;
+                fieldsMap[x, y].BackColor = Color.White;
+                openToWin--;
+                if (fieldsMap[x, y].Name == "")
+                {
+                    for (int xx = x - 1; xx <= x + 1; xx++)
+                    {
+                        for (int yy = y - 1; yy <= y + 1; yy++)
+                        {
+                            if (xx >= 0 && xx < widthMap &&
+                               yy >= 0 && yy < heightMap)
+                            {
+                                OpenField(xx, yy);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OpenAllFields()
+        {
+            for (int x = 0; x < widthMap; x++)
+            {
+                for (int y = 0; y < heightMap; y++)
+                {
+                    fieldsMap[x, y].Text = fieldsMap[x, y].Name;
+                    if (fieldsMap[x, y].Name == "@")
+                    {
+                        if (openToWin == 0)
+                        {
+                            fieldsMap[x, y].BackColor = Color.LightSalmon;
+                        }
+                        else
+                        {
+                            fieldsMap[x, y].BackColor = Color.Red;
+                        }
+                    }
+                    else
+                    {
+                        if (openToWin == 0)
+                        {
+                            fieldsMap[x, y].BackColor = Color.White;
+                        }
+                        else
+                        {
+                            fieldsMap[x, y].BackColor = Color.LightSalmon;
+                        }
+                    }
+                }
+            }
+        }
+        private void małaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gameSize = GameSize.Small;
+            Generate(8, 8, 10);
+        }
+
+        private void średniaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gameSize = GameSize.Medium;
+            Generate(16, 16, 40);
+        }
+
+        private void dużaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gameSize = GameSize.Big;
+            Generate(30, 16, 99);
+        }
+    }
+}
